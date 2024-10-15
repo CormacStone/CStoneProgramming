94e785e2333637ef00f38a13e9ac1e9f179148ed
CormacStone <142530101+CormacStone@users.noreply.github.com>
2024-10-15 07:44:52 -0600

    Update MyProgram.java

--- src/InheritanceApp/MyProgram.java
+++ src/InheritanceApp/MyProgram.java
@@ -1,1 +1,14 @@
+//Cormac Stone
 
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of _____
+        ______ myAnimal = new _______("", "", , , );
+
+        // Call methods
+        myTesla.startEngine();       // Inherited from Vehicle
+        myTesla.honk();              // Inherited from Car
+        myTesla.chargeBattery();     // Specific to ElectricCar
+        myTesla.showInfo();          // Overridden to show details from all levels
+    }
+}
