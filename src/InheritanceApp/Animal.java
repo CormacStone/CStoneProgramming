40a4a40df02db0f03326339efffe59cc342973f8
CormacStone <142530101+CormacStone@users.noreply.github.com>
2024-10-15 08:03:24 -0600

    Update Animal.java

--- src/InheritanceApp/Animal.java
+++ src/InheritanceApp/Animal.java
@@ -1,1 +1,29 @@
+//Cormac Stone
 
+class Animal {
+    private String Name;
+    private String Color;
+    private String Age;
+
+    public Animal(String Name, String Color, String Age) { 
+        this.Name;
+        this.Color;
+        this.Age;
+    }public String startEngine() {
+        String start = "Starting the engine...";
+	return start;
+    }
+    public String getName() {
+        return Name;
+    }
+    public String getColor() {
+        return Color;
+    }
+    public int getAge() {
+        return Age;
+    }
+    public String animalInfo() {
+	String Info = "Animal: " + Name + ", " + Color + ", Years old: " + Age;
+	return Info;
+    }
+}
