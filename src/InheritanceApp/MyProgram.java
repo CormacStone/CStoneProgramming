1c9e05b182ae55a53dfb9f2d43ea13f26d82579a
CormacStone <142530101+CormacStone@users.noreply.github.com>
2024-10-15 08:04:38 -0600

    Update MyProgram.java

--- src/InheritanceApp/MyProgram.java
+++ src/InheritanceApp/MyProgram.java
@@ -2,13 +2,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Create an instance of _____
         ______ myAnimal = new _______("", "", , , );
 
         // Call methods
-        myTesla.startEngine();       // Inherited from Vehicle
-        myTesla.honk();              // Inherited from Car
-        myTesla.chargeBattery();     // Specific to ElectricCar
-        myTesla.showInfo();          // Overridden to show details from all levels
+        myAnimal.startEngine();       // Inherited from Vehicle
+        myAnimal.honk();              // Inherited from Car
+        myAnimal.chargeBattery();     // Specific to ElectricCar
+        myAnimal.Info();          // Overridden to show details from all levels
     }
 }
